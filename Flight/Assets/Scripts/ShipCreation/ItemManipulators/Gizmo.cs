fd168336c0c8d9e50a62d51ebd70b918e1bbdb70
Regalith <tsbujnevicie@wpi.edu>
2015-01-20 02:38:57 -0500

    No clue

--- Flight/Assets/Scripts/ShipCreation/ItemManipulators/Gizmo.cs
+++ Flight/Assets/Scripts/ShipCreation/ItemManipulators/Gizmo.cs
@@ -6,10 +6,49 @@ public class Gizmo : MonoBehaviour {
 	private GameObject translate;
 	private GameObject rotate;
 
+	private GameObject buildCam;
+	private float buildCamDistance;
+
+	public float minScale;
+	public float maxScale;
+
+	public float minDistance;
+	public float maxDistance;
+	//private Vector3 currentScale;
+
+
 	void Start()
 	{
 		translate = transform.Find ("TranslateGizmo").gameObject;
 		rotate = transform.Find ("RotateGizmo").gameObject;
+		buildCam = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+
+	void Update()
+	{
+		ResizeGizmo();
+	}
+
+
+	private void ResizeGizmo()
+	{
+		float tempDistance = Vector3.Distance(this.transform.position, buildCam.transform.position);
+
+		//Exit function if no change in distance between camera and gizmo
+		if(tempDistance == buildCamDistance)
+			return;
+		else
+			buildCamDistance = Mathf.Clamp(tempDistance,minDistance,maxDistance);
+
+
+		Vector3 tempScale = this.transform.localScale;
+
+		tempScale.x = Mathf.Clamp(buildCamDistance,minScale, maxScale);
+		tempScale.y = Mathf.Clamp(buildCamDistance,minScale, maxScale);
+		tempScale.z = Mathf.Clamp(buildCamDistance,minScale, maxScale);
+
+		this.transform.localScale = tempScale;
+
 	}
 
 	public void TurnOnTranslate()
